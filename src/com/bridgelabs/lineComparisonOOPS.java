f8ef36875d6b63dd7e6b63f81e8fa735619605f1
Rajupuli15 <Rajupuli15@gmail.com>
2021-09-16 01:52:39 -0400

    Added:UC1

--- src/com/bridgelabs/lineComparisonOOPS.java
+++ src/com/bridgelabs/lineComparisonOOPS.java
@@ -1,8 +1,29 @@
 package com.bridgelabs;
 
+import java.util.Scanner;
 public class lineComparisonOOPS {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the coordinates (x1,y1)");
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+
+        System.out.println("Enter the coordinates (x2,y2)");
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int res1 = (int) Math.pow((x2-x1), 2);
+        int res2 = (int) Math.pow((y2-y1), 2);
+
+        double Length =  Math.sqrt(res1+res2);
+
+        System.out.println("Lenght of line:"+ Length);
+
     }
 }
+
+    // public static void main(String[] args) {
+        // System.out.println("Welcome to Line Comparison");
+
